a5d3f4ebd02ec89cb8373b6c51e17cce002a867c
AllyB23 <marlin.rosales@ucas-edu.net>
2026-01-07 09:42:56 -0700

    notes

---
+++ notes/decorator_functions.py
@@ -0,0 +1,20 @@
+# MR Notes
+
+def decorator(func):
+    def wrapper():
+        print("Before calling the function. ")
+        func()
+        print("after calling the function.")
+    return wrapper
+
+@decorator
+def greet():
+          print("Hello, world")
+
+greet()
+
+@decorator
+def add():
+    print(1+1)
+
+add()
