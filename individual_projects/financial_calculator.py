17a13957a47a4bd0c8cb2fa8f00196fe5e7444cd
AllyB23 <marlin.rosales@ucas-edu.net>
2026-01-08 09:51:38 -0700

    gooooo

--- individual_projects/financial_calculator.py
+++ individual_projects/financial_calculator.py
@@ -1,3 +1,13 @@
 # MR 1st Financial calculator
+# MAIN FUNCTION
+def main():
+    print("What function would you like to perform?\n 1. Budget allocator\n 2. Sale Price Calcultor\n 3. Tip calculator\n 4. Compound Interest Calculator")
+    #if 
+    pass
+# INNER FUNCTION
+def calcualte_tip():
+    pass
+def sale_price():
+    pass
 
-budget_allocator = []
+main()
