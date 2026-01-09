fdb9a9f5bd8df90d32d0d80864775ffe6d3ca946
AllyB23 <marlin.rosales@ucas-edu.net>
2026-01-08 18:52:20 -0700

    Update financial_calculator.py

--- individual_projects/financial_calculator.py
+++ individual_projects/financial_calculator.py
@@ -1,13 +1,21 @@
 # MR 1st Financial calculator
-# MAIN FUNCTION
+# MAIN FUNCTION (Lobby of my program)
 def main():
     print("What function would you like to perform?\n 1. Budget allocator\n 2. Sale Price Calcultor\n 3. Tip calculator\n 4. Compound Interest Calculator")
-    #if 
+    if 
     pass
-# INNER FUNCTION
+# INNER FUNCTIONs?
 def calcualte_tip():
     pass
-def sale_price():
+def sale_price(): 
     pass
-
+def budget_allocator():
+    pass
+def compound_interest():
+    pass
 main()
+
+calcualte_tip = 3
+budget_allocator = 1
+sale_price = 2
+compound_interest = 4
