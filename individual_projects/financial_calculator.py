8629503378d97d9dc6301aa4d1b97d84dd0a028a
AllyB23 <marlin.rosales@ucas-edu.net>
2026-01-12 09:51:09 -0700

    uppies

--- individual_projects/financial_calculator.py
+++ individual_projects/financial_calculator.py
@@ -8,9 +8,42 @@ function = True
 #def main():
     #print("What function would you like to perform?\n 1. Budget allocator\n 2. Sale Price Calcultor\n 3. Tip calculator\n 4. Compound Interest Calculator")
     #if function == False:
-    
-    #pass
+def main():
+
+    while True:
+        print("Welcome to the MR 1st Financial Calculator.")
+        print("What function would you like to perform?")
+        print(" 1. Savings Time Calculator")
+        print(" 2. Budget allocator")
+        print(" 3. Sale Price Calculator")
+        print(" 4. Tip calculator")
+        print(" 5. Compound Interest Calculator")
+        print(" 6. Exit.")
+
+        choice = input("Enter the number of your choice: ")
+        if choice == '1':
+            saving()
+        elif choice == '2':
+            budget_allocator()
+        elif choice == '3':
+            sale_price()
+        elif choice == '4':
+            calculate_tip()
+        elif choice == '5':
+            compound_interest()
+        elif choice == '6':
+            print("Thank you, Bye.")
+            break
+        else:
+            print("Invalid choice. Please enter a number between 1 and 4.")
 # INNER FUNCTION
+def saving():
+    float(input("What amount are you saving to? "))
+    float(input("How aften are you contributing? "))
+    print(" 1. Weekly\n 2. Monthly")
+    float(input("How much are you contributing each time? "))
+    print("")
+          
 def calculate_tip():
     try:
         bill = float(input("what is the total you want to calculate? "))    
@@ -24,18 +57,30 @@ def sale_price():
     try:
         cost = float(input("How much does the item originally cost? "))
         discount = float(input("What percent is the discount? "))
-        print("The item now costs {cost*discount}")
-    pass
+        discount_amount = cost * (discount / 100)
+        final_price = cost - discount_amount
+        print(f"The item now costs ${final_price:.2f} (You saved ${discount_amount:.2f}).")
+    except ValueError:
+        print("Please enter a valid number.")
 def budget_allocator():
     try:
         categories = int(input("How many budget categories do you have: "))
-        category_1 = rent/mortgage
-        category_2 = transportation
-        category_3 = groceries
-        category_4 = utlilities
-        category_5 = savings
-        monthly_income = float(input("What is your monthly income: "))
+        categories_list = ["category_1 = rent/mortgage",
+        "category_2 = transportation",
+        "category_3 = groceries",
+        "category_4 = utlilities",
+        "category_5 = savings"]
+        monthly_income() = float(input("What is your monthly income: "))
+        rent_mortgage_percent() = float(input("What percent i syour Rent/Mortgage?"))
+        transportation_percent() = float(input("What perent is your transportation?"))
+        groceries_percent() = float(input("What percent is your Utilities? "))
+        savings_percent() = float(input("What percent is your savings?" ))
+        if categories_list == '1':
+            rent_mortgage_percent() 
 
+
+    except ValueError:
+        print("Please enter a valid number.")
 
 
     pass
@@ -50,4 +95,4 @@ def compound_interest():
     pass
 
 
-compound_interest()
+main()
---
+++ notes/list_types.py
@@ -0,0 +1,21 @@
+# MR 2nd List Types Notes
+
+siblings = ["Alex", "Katie", "Andrew", "Tia", "treyson", "Xavier", "Jake"]
+
+print(siblings[3])
+siblings[-2] = "DOW"
+
+print(siblings)
+
+
+fruit = ("apple", "orange", "peach", "Kiwi", "raspberry")
+home = (0,0)
+x,y = home
+
+#fruit[3] = "pineapple"
+print(x)
+
+#set
+colors = {"Orange", "purple", "green", "Blue", "Yellow", "red"}
+
+print(colors)
