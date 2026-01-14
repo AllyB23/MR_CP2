81a9b6c2f14ab067de2261c405467e2805569cea
AllyB23 <marlin.rosales@ucas-edu.net>
2026-01-14 09:38:42 -0700

    pushies

---
+++ individual_projects/personal_library.py
@@ -0,0 +1,30 @@
+# MR 2nd Personal Library
+
+def main():
+    choice = float(input())
+    if choice == "1":
+        view()
+    elif choice == "2":
+        add()
+    elif choice == "3":
+        remove()
+    elif choice == "4":
+        search()
+    elif choice == "5":
+        end()
+
+
+    pass
+def view():
+    list = []
+    pass
+def add():
+    list = []
+    pass
+def remove():
+    list = []
+    pass
+def search():
+    list = []
+    pass
+main()
