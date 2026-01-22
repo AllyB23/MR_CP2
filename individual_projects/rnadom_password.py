c737c105eff9fa71830b94e7c794284751912ee9
AllyB23 <marlin.rosales@ucas-edu.net>
2026-01-22 09:51:31 -0700

    pushies

--- individual_projects/rnadom_password.py
+++ individual_projects/rnadom_password.py
@@ -2,6 +2,25 @@
 import random
 
 def main():
-    pass
+    while True:
+        print("Welcome to the random Password Generator...")
+        print("You are able to generate random passwords...")
+        print("with the requirements you need!")
+        print("Would you like to continue or leave?")
+        print("1. Continue to generate passwords")
+        print("2. Leave")
+
+        if choice == '1':
+            requirements()
+        elif choice == '2':
+            break
+        else:
+            print("Please enter a valid value.")
+        pass
 def requirements():
+    """How long does the password need to be: 
+    Does the password need lowercase letters:
+    Does the passeords need uppercase letters:
+    Does the passwords need numbers letters:
+    Possible passwords: """
     pass
