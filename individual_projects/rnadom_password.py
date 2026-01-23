9034cea6269ea2d07c59f015064dcd696c9ee06c
AllyB23 <marlin.rosales@ucas-edu.net>
2026-01-23 09:37:52 -0700

    bunnies

--- individual_projects/rnadom_password.py
+++ individual_projects/rnadom_password.py
@@ -6,10 +6,10 @@ def main():
         print("Welcome to the random Password Generator...")
         print("You are able to generate random passwords...")
         print("with the requirements you need!")
-        print("Would you like to continue or leave?")
+        choice = ("Would you like to continue or leave?")
         print("1. Continue to generate passwords")
         print("2. Leave")
-
+        
         if choice == '1':
             requirements()
         elif choice == '2':
@@ -23,4 +23,27 @@ def requirements():
     Does the passeords need uppercase letters:
     Does the passwords need numbers letters:
     Possible passwords: """
+    lower = ("Does the password need lower case letters(Y/N): ")
+    upper = ("Does the password need upper case letters(Y/N): ")
+    numbers = ("Does the password need numbers letter(Y/N): ")
+    special_characters = ("Does the password need special characters letters(Y/N): ")
+
+    while True:
+        if lower == 'y':
+        elif lower == 'n':
+        else:
+            break
+        if upper == 'y':
+        elif upper == 'n':
+        else:
+            break
+        if numbers == 'y':
+        elif numbers == 'n':
+        else:
+            break
+        if special_characters == 'y':
+        elif special_characters == 'n':
+        else:
+            break
+
     pass
