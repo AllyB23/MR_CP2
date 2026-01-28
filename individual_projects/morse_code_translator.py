ab211487eec894903a53ac607a141e90e64b799b
AllyB23 <marlin.rosales@ucas-edu.net>
2026-01-28 09:37:42 -0700

    push

---
+++ individual_projects/morse_code_translator.py
@@ -0,0 +1,11 @@
+# MR 1st Morse Code Translator
+# One tuple for the letters in English
+alphabet = ("a", "B", "C", "D", "E", "F", "G", "H","I", "J", "K", "L", "M", "N", "O", "P", "Q","R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+# One tuple for the Morse code letters
+morse_code = (".-", "-...", "-.-.", "-..", ".", "..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-","-.-.","...","-","..-","..-","...-",".--","-..-","-.--","--..")
+# 3 functions
+# function for translating Enlgish to Morse code
+# Another function translating morse code into english
+# Main loop that is a function that lets users choose what to do
+# Morse code to English or exit
+# be able to check for invalid inputs
