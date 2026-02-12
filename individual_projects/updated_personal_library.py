2efb668446d61be71defd14e60e9b440bc3f9175
AllyB23 <marlin.rosales2ucas-edu.net>
2026-02-12 09:37:56 -0700

    Donuts

---
+++ individual_projects/updated_personal_library.py
@@ -0,0 +1,14 @@
+# MR 2nd Personal Library
+# Functions for all of the actions, we will use nested loops and functionds
+
+# Function for Showing the list of the book
+# The simple list
+# the detailed list
+
+# Function for adding an item
+# within this function
+    #function for deleting an item
+# function for updating and item within the adding function
+
+# main function that lets you choose what to do and introduces you to your personal library
+
