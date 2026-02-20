9309125374e20f8105048bcaa19a23400ed6d7a4
AllyB23 <marlin.rosales2ucas-edu.net>
2026-02-20 09:47:08 -0700

    pushies

---
+++ individual_projects/word_counter_main.py
@@ -0,0 +1,11 @@
+# MR 1st Word Counter assignment 20 points
+# Main file
+
+# This main file should be in charge of running the program - acting as a main function?
+
+# MAIN FUNCTION
+    # Welcome the user into the Document word Counter
+    # print statements to introduce and explain to the user how the program works
+
+
+
---
+++ individual_projects/word_counter_time_handling.py
@@ -0,0 +1,3 @@
+# MR 1st word counter time handler file
+
+# Figure out how to use this file???
