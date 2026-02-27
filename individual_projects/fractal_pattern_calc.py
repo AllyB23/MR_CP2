15e605cc12e955052cd9b08894e13998241fe605
AllyB23 <marlin.rosales2ucas-edu.net>
2026-02-27 09:47:16 -0700

    pseudo

--- individual_projects/fractal_pattern_calc.py
+++ individual_projects/fractal_pattern_calc.py
@@ -1,3 +1,31 @@
 # MR 1st Fractal Pattern calculator
+# Import the Turtle Module
 
-#
+# Create the turtle
+# Give it a starting point
+
+
+
+# MAIN FUNCTION 
+# Main function will let the user run the program and will handle user inputs
+
+# DEF MAIN FUNCTION
+def main():
+    # Welcome and explain the program to the user using print statements
+    print("welcome to the Fractal Pattern Calculator!")
+    print("This Program can generate a Sierpinski Triangle Fractal Pattern!")
+    # Tell them the options they have to choose from    
+    print("Here you have the option to start or leave...")
+    print(" 1. Start the program")
+    print(" 2. Leave")
+
+"""Welcome to the Fractal Pattern calculator etc."""
+while True:
+    # make a WHILE TRUE loop so the function keeps going until they choose the option to exit the program which breaks the loop
+    # Choice = user input
+        # IF choice - '1'
+            # Take them to option1
+        # IF choice - '2'
+            # Take them to to option 2
+
+        
