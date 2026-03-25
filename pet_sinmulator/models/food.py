29c309529c1f77f85eccb0320096f6296fa03a5c
AllyB23 <marlin.rosales@ucas-edu.net>
2026-03-24 23:54:37 -0600

    Update food.py

--- pet_sinmulator/models/food.py
+++ pet_sinmulator/models/food.py
@@ -1,1 +1,12 @@
+class Food:
+    def __init__(self, name, hunger, happiness, price):
+        self.name = name
+        self.hunger = hunger
+        self.happiness = happiness
+        self.price = price
 
+foods = [
+    Food("Kibble", 20, 5, 0),
+    Food("Premium", 30, 15, 5),
+    Food("Treat", 10, 25, 3),
+]
