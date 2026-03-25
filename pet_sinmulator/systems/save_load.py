e59ef4179e559a8d1761afebdbcdf2dbbe1c7cf3
AllyB23 <marlin.rosales@ucas-edu.net>
2026-03-24 23:52:18 -0600

    Update save_load.py

--- pet_sinmulator/systems/save_load.py
+++ pet_sinmulator/systems/save_load.py
@@ -1,1 +1,17 @@
+import json
+from models.pet import Pet
 
+def save_game(pet):
+    with open("data/saves.json", "w") as f:
+        json.dump(pet.__dict__, f)
+
+def load_game():
+    try:
+        with open("data/saves.json", "r") as f:
+            data = json.load(f)
+            pet = Pet(data["name"], data["species"])
+            pet.__dict__.update(data)
+            return pet
+    except:
+        print("No save file found.")
+        return None
