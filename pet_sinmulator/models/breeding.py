ba85f91869db4c132e7be3e042bcf1780a4c4477
AllyB23 <marlin.rosales@ucas-edu.net>
2026-03-25 00:01:07 -0600

    Update breeding.py

--- pet_sinmulator/models/breeding.py
+++ pet_sinmulator/models/breeding.py
@@ -1,1 +1,9 @@
+from models.pet import Pet
 
+def breed(pet1, pet2):
+    name = pet1.name + pet2.name
+    baby = Pet(name, pet1.species)
+
+    baby.happiness = (pet1.happiness + pet2.happiness) // 2
+
+    return baby
