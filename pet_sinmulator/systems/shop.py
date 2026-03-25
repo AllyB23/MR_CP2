f8d28b870e9017dab8aad5953b1c7287ab1768f8
AllyB23 <marlin.rosales@ucas-edu.net>
2026-03-24 23:54:58 -0600

    Update shop.py

--- pet_sinmulator/systems/shop.py
+++ pet_sinmulator/systems/shop.py
@@ -1,1 +1,7 @@
-
+def buy_food(pet, food):
+    if pet.money >= food.price:
+        pet.money -= food.price
+        pet.inventory.append(food)
+        print(f"Bought {food.name}")
+    else:
+        print("Not enough money!")
