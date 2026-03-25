5d8e028ce664813e89344c33e79cfe6b7b66e84b
AllyB23 <marlin.rosales@ucas-edu.net>
2026-03-24 23:50:37 -0600

    Update events.py

--- pet_sinmulator/systems/events.py
+++ pet_sinmulator/systems/events.py
@@ -1,1 +1,13 @@
+import random
 
+def random_event(pet):
+    events = [
+        ("found a toy", 10),
+        ("got sick", -10),
+        ("made a friend", 15),
+    ]
+
+    event = random.choice(events)
+    pet.happiness += event[1]
+
+    print(f"\n🎉 EVENT: {pet.name} {event[0]}! ({event[1]} happiness)")
