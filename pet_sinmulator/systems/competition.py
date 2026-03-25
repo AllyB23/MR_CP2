3f822b58b35914cc7c2aa5a1ca79d0ec9d010cf0
AllyB23 <marlin.rosales@ucas-edu.net>
2026-03-24 23:57:14 -0600

    Update competition.py

--- pet_sinmulator/systems/competition.py
+++ pet_sinmulator/systems/competition.py
@@ -1,1 +1,12 @@
+import random
 
+def compete(pet):
+    score = random.randint(0, 100)
+    print(f"{pet.name} scored {score}!")
+
+    if score > 60:
+        pet.level += 1
+        pet.money += 10
+        print(" You won! +Level +$10")
+    else:
+        print(" You lost!")
