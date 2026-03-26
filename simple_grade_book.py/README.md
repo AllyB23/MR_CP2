2f3ad9ea5160c1cbdb9dfe716c0251ae9f1c5311
AllyB23 <marlin.rosales@ucas-edu.net>
2026-03-26 09:40:56 -0600

    simple gradebook

---
+++ simple_grade_book.py/README.md
@@ -0,0 +1,3 @@
+SIMPLE GRADEBOOK
+
+This is a program that you can use as a gradebook. You can organize the grades of students and it saves all of the information for you
---
+++ simple_grade_book.py/main.py
@@ -0,0 +1,20 @@
+# MR 1st simple grade book
+import csv
+
+class Student:
+    def __init__(self, name, student_id,):
+        self.name = name
+        self.student_id = student_id
+        self.grades =[]
+        pass
+
+student1 =Student("Timmy")
+student2 = Student("Alora")
+student3 = Student("Greg")
+
+
+class GradeBook:
+    def __init__(self, grades):
+        self.grades = grades
+
+    def 
