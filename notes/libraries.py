0b37aabdc2ecd801d22c126437095e82b9544915
AllyB23 <marlin.rosales@ucas-edu.net>
2026-03-30 09:38:54 -0600

    Libraries notes and classes project

---
+++ notes/libraries.py
@@ -0,0 +1,3 @@
+# MR 1st Python Linraries Notes
+import numpy  as np
+a = np.arange(15).reshape(3, 5)
---
+++ simple_grade_book.py/user_interface.py
@@ -0,0 +1,30 @@
+# MR 1st Simple Grade Book User Interface
+# import module aliases
+
+
+def main():
+    while True:
+        print("==============SIMPLE GRADE BOOK===============")
+        print("Welcome to your Simple Grade book...")
+        print("Here you can input grades and student names to keep track of your class grades...")
+        print("This program allows you to search up a student and add students as you please...")
+        print("You can input then with their student ID if they have one or you can serach up by their first and last name...")
+        print("You will be able to input their grades in percentage form and the program will give you their letter grade...")
+
+        print("1. Add student")
+        print("2. View Students")
+        print("3. leave")
+
+        choice = input("What would you like to do? ")
+
+        if choice == '1':
+            pass
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            pass
+        else:
+            return
+        print("Please enter a valid choice")
+
+main()
