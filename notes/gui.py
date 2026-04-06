294373e6e5a39c071ed17264f94127844d0aaf18
AllyB23 <marlin.rosales@ucas-edu.net>
2026-04-06 09:49:15 -0600

    notes

---
+++ notes/gui.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("Testing")
+root.configure(background = "orange")
+root.minsize(250, 250)
+root.maxsize(1000, 1000)
+root.geometry("300x300+100+100")
+label = tk.Label(root, text = "This is currently working!", font = ("Times New Roman", 14, "bold"))
+label.config(fg = "blue", background = "orange")
+# stuff about buttons
+root.count = 0
+def add():
+    count += 1
+    tk.Label(root, text = root.count)
+
+
+btn = tk.Button(root, text = "ADD", command = add)
+btn.pack()
+
+
+label.pack()
+#image = tk.PhotoImage(file = "img\\tea.png")
+#tk.Label(root, image = image).pack()
+
+
+root.mainloop()
