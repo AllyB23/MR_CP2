74836786e6ca6df79ce681293eb35e521c8455cd
AllyB23 <marlin.rosales@ucas-edu.net>
2026-04-06 23:41:00 -0600

    Update generator.py

--- rpg updated/models/generator.py
+++ rpg updated/models/generator.py
@@ -1,1 +1,18 @@
+import random
+from faker import Faker
+from models.character import Character
 
+class RandomGenerator:
+    def __init__(self):
+        self.fake = Faker()
+        self.races = ("Human", "Elf", "Orc")
+        self.classes = ("Fighter", "Wizard", "Rogue")
+
+    def generate_random_character(self):
+        name = self.fake.name()
+        race = random.choice(self.races)
+        char_class = random.choice(self.classes)
+        level = random.randint(1, 15)
+        # Generate random stats between 3 and 18
+        attributes = [random.randint(3, 18) for _ in range(4)]
+        return Character(name, race, char_class, level, attributes)
